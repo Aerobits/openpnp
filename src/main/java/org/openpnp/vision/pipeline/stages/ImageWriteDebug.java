835de8ab799ff8d09d984014f74623f225a73eaf
aerobits1 <aerobits1@aerobits1>
2021-05-28 10:16:32 +0200

    Improved ImageWriteDebug files naming

--- src/main/java/org/openpnp/vision/pipeline/stages/ImageWriteDebug.java
+++ src/main/java/org/openpnp/vision/pipeline/stages/ImageWriteDebug.java
@@ -1,12 +1,20 @@
 package org.openpnp.vision.pipeline.stages;
 
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.prefs.Preferences;
 
 import org.opencv.imgcodecs.Imgcodecs;
+import org.openpnp.gui.JobPanel;
 import org.openpnp.model.Configuration;
+import org.openpnp.model.Part;
+import org.openpnp.spi.Nozzle;
 import org.openpnp.util.LogUtils;
 import org.openpnp.vision.pipeline.CvPipeline;
 import org.openpnp.vision.pipeline.CvStage;
+import org.pmw.tinylog.Logger;
 import org.simpleframework.xml.Attribute;
 
 /**
@@ -35,14 +43,37 @@ public class ImageWriteDebug extends CvStage {
     public void setSuffix(String suffix) {
         this.suffix = suffix;
     }
-
+ 
     @Override
     public Result process(CvPipeline pipeline) throws Exception {
         if (!LogUtils.isDebugEnabled()) {
             return null;
         }
-        File file = Configuration.get().createResourceFile(getClass(), prefix, suffix);
-        Imgcodecs.imwrite(file.getAbsolutePath(), pipeline.getWorkingImage());
-        return null;
-    }
+        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd_HH.mm.ss.SS");
+		LocalDateTime now = LocalDateTime.now();
+		String nozzleAndPartInfo = ""; // String.format("%s_%s", dtf.format(now), prefix);
+
+		Nozzle n = (Nozzle) pipeline.getProperty("nozzle");
+		if (n != null) {
+			nozzleAndPartInfo += String.format("_%s", n.getId());
+		}
+		
+		Part p = (Part) pipeline.getProperty("part");
+		if (p != null) {
+			nozzleAndPartInfo += String.format("_%s", p.getId());
+		}
+
+		File directory = new File(Configuration.get().getConfigurationDirectory(), getClass().getSimpleName());
+		if (!directory.exists()) {
+			directory.mkdirs();
+		}
+
+		// TODO: with prefix it doesn't work - multiple datetimes in filename
+		String filename = String.format("%s%s%s", dtf.format(now), nozzleAndPartInfo, suffix);
+		File file = new File(directory, filename);
+		Imgcodecs.imwrite(file.getAbsolutePath(), pipeline.getWorkingImage());
+
+		return null;
+	}
 }
