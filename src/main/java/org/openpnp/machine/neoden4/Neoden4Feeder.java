49581827707713d954640903ebeb4012f1015037
CezaryPuchowksi <puchowski.cezary@gmail.com>
2024-09-18 15:05:40 +0200

    Add some fields to neoden feeder and to feeder class. Bind it up

--- src/main/java/org/openpnp/machine/neoden4/Neoden4Feeder.java
+++ src/main/java/org/openpnp/machine/neoden4/Neoden4Feeder.java
@@ -25,25 +25,38 @@ import org.openpnp.spi.Nozzle;
 import org.openpnp.spi.PropertySheetHolder;
 import org.openpnp.spi.VisionProvider;
 import org.pmw.tinylog.Logger;
+import org.python.antlr.PythonParser.return_stmt_return;
+import org.python.antlr.base.boolop;
 import org.python.modules.thread.thread;
 import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.core.Persist;
 
+import jnr.ffi.types.int8_t;
+
 
 public class Neoden4Feeder extends ReferenceFeeder {
 
     private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
-    
+
     @Attribute(required = false)
     protected String actuatorName;
 
     @Attribute(required = false)
     private int feedCount = 0;
-    
+
     @Attribute(required = false)
     private int discardCount = 0;
-    
+
+    @Attribute(required = false)
+    private boolean suspendState = false;
+
+    @Attribute(required = false)
+    private int suspendTries = 3;
+
+    @Attribute(required = false)
+    private int suspendThreshold = 10;
+
     @Element(required = false)
     protected Vision vision = new Vision();
 
@@ -84,19 +97,19 @@ public class Neoden4Feeder extends ReferenceFeeder {
 					String.format("No Actuator found with name %s on feed Head %s", actuatorName, head.getName()));
 		}
 
-        // Actuate actuator 
+        // Actuate actuator
     	actuator.actuate(getPart().getPitchInTape());
 
     	// Calculate vision offset
-    	
-    	
+
+
         if (vision.isEnabled()) {
         	try {
         		visionOffset = getVisionOffsets(head, location);
                 Logger.debug("final visionOffsets " + visionOffset);
-                Logger.debug("Modified pickLocation {}", getPickLocation());	
+                Logger.debug("Modified pickLocation {}", getPickLocation());
         	} catch (Exception e) {
-        		
+
 			}
         }
 
@@ -105,7 +118,7 @@ public class Neoden4Feeder extends ReferenceFeeder {
 
     private Location getVisionOffsets(Head head, Location pickLocation) throws Exception {
         Logger.debug("getVisionOffsets({}, {})", head.getName(), pickLocation);
-        
+
         // Find the Camera to be used for vision
         Camera camera = null;
         for (Camera c : head.getCameras()) {
@@ -117,11 +130,11 @@ public class Neoden4Feeder extends ReferenceFeeder {
         if (camera == null) {
             throw new Exception("No vision capable camera found on head.");
         }
-        
+
         if (vision.getTemplateImage() == null) {
             throw new Exception("Template image is required when vision is enabled.");
         }
-        
+
         if (vision.getAreaOfInterest().getWidth() == 0 || vision.getAreaOfInterest().getHeight() == 0) {
             throw new Exception("Area of Interest is required when vision is enabled.");
         }
@@ -138,11 +151,11 @@ public class Neoden4Feeder extends ReferenceFeeder {
         VisionProvider visionProvider = camera.getVisionProvider();
 
         // Convert AOI origin to top-left corner (Neoden4Camera changes resolution)
-        Rectangle vision_aoi = getVision().getAreaOfInterest();        
+        Rectangle vision_aoi = getVision().getAreaOfInterest();
 		Rectangle aoi = new Rectangle(
-				vision_aoi.getX() + (camera.getWidth() / 2), 
-				vision_aoi.getY() + (camera.getHeight() / 2), 
-				vision_aoi.getWidth(), 
+				vision_aoi.getX() + (camera.getWidth() / 2),
+				vision_aoi.getY() + (camera.getHeight() / 2),
+				vision_aoi.getWidth(),
 				vision_aoi.getHeight());
 
 
@@ -150,47 +163,47 @@ public class Neoden4Feeder extends ReferenceFeeder {
 		// - user set camera tp 1024x1024
 		// - user select AOI almost full screen and save
 		// - when camera is 512x512 and feeder is checking
-		//   vision offsets, vision_aoi can be < 0 or > 512 
+		//   vision offsets, vision_aoi can be < 0 or > 512
 		//   and cause openCV error
 		// If there is, clamp AOI to 512x512
         if (aoi.getX() < 0) {
-        	aoi.setX(0); 
+        	aoi.setX(0);
         }
         if (aoi.getX() > 512) {
-        	aoi.setX(512); 
+        	aoi.setX(512);
         }
         if (aoi.getY() < 0) {
-        	aoi.setY(0); 
+        	aoi.setY(0);
         }
         if (aoi.getY() > 512) {
-        	aoi.setY(512); 
+        	aoi.setY(512);
         }
         if (aoi.getWidth() < 0) {
-        	aoi.setWidth(0); 
+        	aoi.setWidth(0);
         }
         if (aoi.getWidth() > 512) {
-        	aoi.setWidth(512); 
+        	aoi.setWidth(512);
         }
         if (aoi.getHeight() < 0) {
-        	aoi.setHeight(0); 
+        	aoi.setHeight(0);
         }
         if (aoi.getHeight() > 512) {
-        	aoi.setHeight(512); 
+        	aoi.setHeight(512);
         }
-		
+
 		// Perform the template match
 		Logger.debug("Perform template match.");
 		Logger.debug(String.format("AOI X:%d, Y:%d, W:%d, H:%d",
 				aoi.getX(), aoi.getY(), aoi.getWidth(), aoi.getHeight()));
-		
+
 		try {
 			Point[] matchingPoints = visionProvider.locateTemplateMatches(
-					aoi.getX(), aoi.getY(), aoi.getWidth(), aoi.getHeight(), 
+					aoi.getX(), aoi.getY(), aoi.getWidth(), aoi.getHeight(),
 					0, 0, vision.getTemplateImage());
-		
+
 			// Get the best match from the array
 	        Point match = matchingPoints[0];
-	
+
 	        // match now contains the position, in pixels, from the top left corner
 	        // of the image to the top left corner of the match. We are interested in
 	        // knowing how far from the center of the image the center of the match is.
@@ -200,36 +213,36 @@ public class Neoden4Feeder extends ReferenceFeeder {
 	        double templateHeight = vision.getTemplateImage().getHeight();
 	        double matchX = match.x;
 	        double matchY = match.y;
-	
+
 	        Logger.debug("matchX {}, matchY {}", matchX, matchY);
-	
+
 	        // Adjust the match x and y to be at the center of the match instead of
 	        // the top left corner.
 	        matchX += (templateWidth / 2);
 	        matchY += (templateHeight / 2);
-	
+
 	        Logger.debug("centered matchX {}, matchY {}", matchX, matchY);
-	
+
 	        // Calculate the difference between the center of the image to the
 	        // center of the match.
 	        double offsetX = (imageWidth / 2) - matchX;
 	        double offsetY = (imageHeight / 2) - matchY;
-	
+
 	        Logger.debug("offsetX {}, offsetY {}", offsetX, offsetY);
-	
+
 	        // Invert the Y offset because images count top to bottom and the Y
 	        // axis of the machine counts bottom to top.
 	        offsetY *= -1;
-	
+
 	        Logger.debug("negated offsetX {}, offsetY {}", offsetX, offsetY);
-	
+
 	        // And convert pixels to units
 	        Location unitsPerPixel = camera.getUnitsPerPixel();
 	        offsetX *= unitsPerPixel.getX();
 	        offsetY *= unitsPerPixel.getY();
-	
+
 	        Logger.debug("final, in camera units offsetX {}, offsetY {}", offsetX, offsetY);
-	
+
 	        return new Location(unitsPerPixel.getUnits(), offsetX, offsetY, 0, 0);
 		}
         catch (Exception e) {
@@ -270,7 +283,7 @@ public class Neoden4Feeder extends ReferenceFeeder {
         this.feedCount = feedCount;
         firePropertyChange("feedCount", oldValue, feedCount);
     }
-    
+
     public int getDiscardCount() {
         return discardCount;
     }
@@ -280,6 +293,30 @@ public class Neoden4Feeder extends ReferenceFeeder {
         int oldValue = this.discardCount;
         this.discardCount = discardCount;
         firePropertyChange("discardCount", oldValue, discardCount);
+    }
+
+    public boolean getSuspendState() {
+        return suspendState;
+    }
+
+    public void setSuspendState(boolean state) {
+        suspendState = state;
+    }
+
+    public int getSuspendTries() {
+        return suspendTries;
+    }
+
+    public void setSuspendTries(int tries) {
+        suspendTries = tries;
+    }
+
+    public int getSuspendThreshold() {
+        return suspendThreshold;
+    }
+
+    public void setSuspendThreshold(int threshold) {
+        suspendThreshold = threshold;
     }
 
     public Vision getVision() {
